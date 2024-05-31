fdee507eb28e5aae19c25ef5790d9e58f4e95e83
Najm-Ur-Rehman <67186639+Najaam@users.noreply.github.com>
2024-05-31 14:29:22 -0700

    .

--- main.cpp
+++ main.cpp
@@ -4,22 +4,6 @@ using namespace MyLibrary;
 
 int main(){
 LinkedList<int> intList;
-    intList.insertAtBeginning(10);
-    intList.insertAtEnd(20);
-    intList.insertAtPosition(15, 1);
-    intList.insertAtPosition(5, 0);
-    intList.insertAtPosition(25, 4); 
-    intList.writeToFile("linked_list.txt");
-
-LinkedList<string> stringList;
-    stringList.insertAtBeginning("hello");
-    stringList.insertAtEnd("world");
-    stringList.insertAtPosition("middle", 1);
-
-    cout << "String List:" << endl;
-    stringList.writeToFile("linked_list.txt");
-    stringList.readFromFile("linked_list.txt");
-
    
 
 }
